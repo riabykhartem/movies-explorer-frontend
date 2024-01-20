6fb4cb1a8273b620f3ea9698663051432c65a740
Artem Riabykh <artemriabykh@Artems-MBP.hsd1.il.comcast.net>
2024-01-20 13:33:24 -0600

    portfolio links updated

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,7 +6,7 @@ export default function Portfolio() {
       <ul className="portfolio__links">
         <li className="portfolio_list-item">
           <a
-            href="https://github.com/riabykhartem/how-to-learn"
+            href=" https://riabykhartem.github.io/how-to-learn"
             target="_blank"
             rel="noreferrer"
             className="portfolio__link link"
@@ -17,7 +17,7 @@ export default function Portfolio() {
         </li>
         <li className="portfolio_list-item">
           <a
-            href="https://github.com/riabykhartem/russian-travel"
+            href="https://riabykhartem.github.io/russian-travel/#"
             target="_blank"
             rel="noreferrer"
             className="portfolio__link link"
@@ -29,7 +29,7 @@ export default function Portfolio() {
 
         <li className="portfolio_list-item">
           <a
-            href="https://github.com/riabykhartem/mesto-react"
+            href="https://mesto.riabykh.nomoredomainsrocks.ru/sign-up"
             target="_blank"
             rel="noreferrer"
             className="portfolio__link link"
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -5,7 +5,7 @@ export default function Promo() {
         <section className="promo">
             <div className="promo__container page__content">
                 <div className="promo__container_place_text">
-                    <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+                    <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
                     <p className="promo__paragraph">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
                     <HashLink  className="promo__link button" smooth to="/#about">Узнать больше</HashLink >
                 </div>
